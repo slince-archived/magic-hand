6ae621e8bce9213ad5832d58d5867aeb3162e493
slince <taosikai@yeah.net>
2016-07-16 16:06:53 +0800

    修改单元测试

--- tests/MagicHandTest.php
+++ tests/MagicHandTest.php
@@ -20,9 +20,15 @@ class MagicHandTest extends \PHPUnit_Framework_TestCase
 
     function testConstruct()
     {
-        $magicHand = new MagicHand('src', 'dst', [300, 200]);
+        $magicHand = new MagicHand('src', 'dst');
         $this->assertInstanceOf('\\Symfony\\Component\\Filesystem\\Filesystem', $magicHand->getFilesystem());
         $this->assertInstanceOf('\\Symfony\\Component\\Finder\\Finder', $magicHand->getFinder());
+    }
+
+    function testSetThumbBox()
+    {
+        $magicHand = new MagicHand('src', 'dst');
+        $magicHand->setThumbBox([300, 200]);
         $this->assertEquals(300, $magicHand->getThumbBox()->getWidth());
         $this->assertEquals(200, $magicHand->getThumbBox()->getHeight());
     }
